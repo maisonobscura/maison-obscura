7ebd4db05acdb5ed0970c354d911c983e20a6eeb
KDFX Team <summerl0v3@mail.ru>
2026-09-25 23:38:39 +0300

    fix image path

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -41,7 +41,7 @@ function About() {
         <div className="about__photo">
 
           <img
-            src="/img/me.jpeg"
+            src={'${import.meta.env.BASE_URL}img/me.jpeg'}
             alt="maison obscura"
           />
 
